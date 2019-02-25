94bfeca651d922c5f349e62dac48699ebc41341b
Arli Machado <arli.machado@gmail.com>
2019-02-25 00:23:56 -0300

    Configurando o projeto para rodar

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 module.exports = {
   entry: './src/index.jsx',
   output: {
@@ -9,13 +8,13 @@ module.exports = {
   },
   devServer: {
     port: 8080,
-    contentBase: './public'
+    contentBase: './public',
   },
   resolve: {
-    extensions: ['', 'js', 'jsx'],
+    extensions: ['', '.js', '.jsx'],
     alias: {
       modules: `${__dirname}/node_modules`,
-      jquery: 'modules/admin-lte/plugin/jquery/jquery-2.2.3.min.js',
+      jquery: 'modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
       bootstrap: 'modules/admin-lte/bootstrap/js/bootstrap.js'
     }
   },
@@ -28,22 +27,20 @@ module.exports = {
     new ExtractTextPlugin('app.css')
   ],
   module: {
-    loaders: [
-      {
-        test: /.js[x]?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        query: {
-          presets: ['es2015', 'react'],
-          plugins: ['transform-object-rest-spread']
-        }
-      }, {
-        test: /\.css$/,
-        loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
-      }, {
-        test: /\.woff|.woff2|.ttf|.eot|.svg|.png|.jpg*.*$/,
-        loader: 'file'
+    loaders: [{
+      test: /.js[x]?$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/,
+      query: {
+        presets: ['es2015', 'react'],
+        plugins: ['transform-object-rest-spread']
       }
-    ]
+    }, {
+      test: /\.css$/,
+      loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+    }, {
+      test: /\.woff|.woff2|.ttf|.eot|.svg|.png|.jpg*.*$/,
+      loader: 'file'
+    }]
   }
 }
