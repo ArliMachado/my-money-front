ddb0eb72a7bd85db654f66c18e81faf84a3321a9
Arli Machado <arli.machado@gmail.com>
2019-03-05 16:58:17 -0300

    configurando serviço de autenticacao

---
+++ src/auth/authActions.js
@@ -0,0 +1,48 @@
+import { toastr } from 'react-redux-toastr';
+import axios from 'axios';
+import consts from '../const';
+
+const submit = (values, url) => {
+  return (dispatch) => {
+    axios.post(url, values)
+      .then((resp) => {
+        dispatch([
+          { type: 'USER_FETCHED', payload: resp.data }
+        ]);
+      })
+      .catch((e) => {
+        e.response.data.errors.forEach(
+          error => toastr.error('Erro', error),
+        );
+      });
+  };
+};
+
+const login = (values) => {
+  return submit(values, `${consts.OAPI_URL}/login`);
+}
+const signup = (values) => {
+  return submit(values, `${consts.OAPI_URL}/signup`);
+};
+
+const logout = () => {
+  return { type: 'TOKEN_VALIDATED', payload: false };
+};
+
+const validateToken = (token) => {
+  return (dispatch) => {
+    if (token) {
+      axios.post(`${consts.OAPI_URL}/validateToken`, { token })
+        .then((resp) => {
+          dispatch({ type: 'TOKEN_VALIDATED', payload: resp.data.valid });
+        })
+        .catch(() => dispatch({ type: 'TOKEN_VALIDATED', payload: false }));
+    } else {
+      dispatch({ type: 'TOKEN_VALIDATED', payload: false });
+    }
+  };
+};
+
+export default {
+  login, signup, logout, validateToken,
+};
